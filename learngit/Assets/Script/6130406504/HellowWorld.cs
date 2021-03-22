3cca687689c42be70c498517f83f2b28a73c65c5
KillerzScrubbie <tutteptan@kkumail.com>
2021-03-22 11:19:29 +0700

    Hello World?

--- learngit/Assets/Script/6130406504/HellowWorld.cs
+++ learngit/Assets/Script/6130406504/HellowWorld.cs
@@ -7,7 +7,29 @@ public class HellowWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("KITTIKORN NONGTU KITTIKORN NONGTU");
+        Debug.Log("KITTIKORN NONGTU KITTIKORN NONGTU");
+        Debug.Log("KITTIKORN NONGTU KITTIKORN NONGTU");
+        Debug.Log("KITTIKORN NONGTU KITTIKORN NONGTU");
+        Debug.Log("KITTIKORN NONGTU KITTIKORN NONGTU");
+        Debug.Log("KITTIKORN NONGTU KITTIKORN NONGTU");
+        Debug.Log("KITTIKORN NONGTU KITTIKORN NONGTU");
+        Debug.Log("KITTIKORN NONGTU KITTIKORN NONGTU");
+        Debug.Log("KITTIKORN NONGTU KITTIKORN NONGTU");
+        Debug.Log("KITTIKORN NONGTU KITTIKORN NONGTU");
+        Debug.Log("KITTIKORN NONGTU KITTIKORN NONGTU");
+        Debug.Log("KITTIKORN NONGTU KITTIKORN NONGTU");
+        Debug.Log("KITTIKORN NONGTU KITTIKORN NONGTU");
+        Debug.Log("KITTIKORN NONGTU KITTIKORN NONGTU");
+        Debug.Log("KITTIKORN NONGTU KITTIKORN NONGTU");
+        Debug.Log("KITTIKORN NONGTU KITTIKORN NONGTU");
+        Debug.Log("KITTIKORN NONGTU KITTIKORN NONGTU");
+        Debug.Log("KITTIKORN NONGTU KITTIKORN NONGTU");
+        Debug.Log("KITTIKORN NONGTU KITTIKORN NONGTU");
+        Debug.Log("KITTIKORN NONGTU KITTIKORN NONGTU");
+        Debug.Log("KITTIKORN NONGTU KITTIKORN NONGTU");
+        Debug.Log("KITTIKORN NONGTU KITTIKORN NONGTU");
+        Debug.Log("KITTIKORN NONGTU KITTIKORN NONGTU");
     }
 
     // Update is called once per frame
