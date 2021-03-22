eab15fb458c7ff6c24709a24e2379651f9b27df3
Chakritz134 <58926322+Chakritz134@users.noreply.github.com>
2021-03-22 10:52:55 +0700

    script 6473

---
+++ learngit/Assets/Script/613040647-3/Chakrit6130406473.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chakrit6130406473 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
